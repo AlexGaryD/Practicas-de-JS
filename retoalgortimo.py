cc2f5215b85b8bd8f57234105d86011d38cd1fb0
Alexis Vera S. <alxvcs93@hotmail.com>
2025-05-27 20:03:26 -0500

    creación de otro archivo py

---
+++ retoalgortimo.py
@@ -0,0 +1,12 @@
+#Extraer un substring de una cadena con Phyton
+frase = "One Piece es un anime muy bueno"
+#Extraer un substring de una cadena con Phyton
+# Definimos los índices de inicio y fin del substring   
+inicial = 4
+#Definimos el índice de inicio del substring
+final = 10
+#Definimos el índice de fin del substring
+palabra = frase[inicial:final]
+# Imprimimos el substring extraído
+print("La palabra es:", palabra)
+
