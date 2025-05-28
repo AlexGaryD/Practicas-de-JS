a1fa9acfa178c40a83caab7fbd9c0a2ba09e7266
Alexis Vera S. <alxvcs93@hotmail.com>
2025-05-27 19:43:39 -0500

    creando archivo de Python

---
+++ calcualdora.py
@@ -0,0 +1,33 @@
+#Creación de calculadora con las operaciones básicas
+valor1 = 7
+#Creación de variables
+valor2 = 3
+#Creación de variables
+#Operación de suma
+resultado = valor1 + valor2
+#Impresión del resultado
+print("El resultado de la suma es:", resultado)
+#Operación de resta
+resultado = valor1 - valor2
+#Impresión del resultado
+print("El resultado de la resta es:", resultado)
+#Operación de multiplicación
+resultado = valor1 * valor2
+#Impresión del resultado
+print("El resultado de la multiplicación es:", resultado)
+#Operación de división
+resultado = valor1 / valor2
+#Impresión del resultado
+print("El resultado de la división es:", resultado)
+#Operación de módulo
+resultado = valor1 % valor2
+#Impresión del resultado
+print("El resultado del módulo es:", resultado)
+#Operación de potencia
+resultado = valor1 ** valor2
+#Impresión del resultado
+print("El resultado de la potencia es:", resultado)
+#Operación de división entera
+resultado = valor1 // valor2
+#Impresión del resultado
+print("El resultado de la división entera es:", resultado)
